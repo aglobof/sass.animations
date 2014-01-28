75970182838efab232f36c6d031d3208d52435cb
Robb Schiller <robb.schiller@gmail.com>
2014-01-28 12:50:37 -0500

    working

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ var gulp = require('gulp');
 var sass = require ('gulp-sass');
 
 gulp.task('sass', function () {
-	gulp.src(['animate.scss', 'keyframes.scss'])
+	gulp.src(['main.scss', 'animate.scss'])
 		.pipe(sass())
 		.pipe(gulp.dest(''));
 });
