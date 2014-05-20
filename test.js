33bee86f2e31f0f167fbadfd3fabd8f7bbfd28e5
drocarmo <pe@drocarmo.com>
2014-05-19 21:40:59 -0500

    some js to make the button animate on click

---
+++ test.js
@@ -0,0 +1,8 @@
+// Button click test
+// (WIP) I'll add more to this to make it more dynamic
+
+$(document).ready(function(){
+  $('.btn').click(function(){
+    $(this).addClass('active');
+  });
+});
